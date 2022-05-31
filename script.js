f1e04cb0ab0057038f108fec2971892565ff27c0
updatedMyJournal <99217422+updatedMyJournal@users.noreply.github.com>
2022-05-31 23:40:33 +0300

    Add operate() and display functions

--- script.js
+++ script.js
@@ -15,3 +15,42 @@ let mathMethods = {
     return result;
   },
 };
+
+function operate(op, a, b) {
+  return mathMethods[op](+a, +b);
+}
+
+let operand1 = null;
+let operand2 = null;
+let operator;
+let outputElem = document.querySelector('.output');
+let resultElem = document.querySelector('.result');
+
+function display(value) {
+  outputElem.textContent += value;
+}
+
+function showResult() {
+  resultElem.style.visibility = 'visible';
+}
+
+function setResult(value) {
+  resultElem.textContent = value;
+}
+
+function clearDisplay() {
+  resetOutput();
+  resetResult();
+}
+
+function resetOutput() {
+  outputElem.textContent = '';
+  operand1 = null;
+  operand2 = null;
+  operator = null;
+}
+
+function resetResult() {
+  resultElem.textContent = '';
+  resultElem.style.visibility = '';
+}
